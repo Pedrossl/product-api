854241de88d789d275e6b3edd873b6a19fa51506
Lobato <pedrosslobato@gmail.com>
2025-01-02 20:30:59 -0300

    ♻️ Update Product Entity To Include Name, Category, And Price Fields

--- src/modules/product/entities/product.entity.ts
+++ src/modules/product/entities/product.entity.ts
@@ -1,7 +1,23 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { CategoryEnum } from 'src/common/enuns/category.enum';
+import { BaseEntity } from 'src/modules/bases/entities/base.entity';
+import { Column, Entity } from 'typeorm';
 
-@ObjectType()
-export class Product {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+@Entity('product')
+export class Product extends BaseEntity {
+  @Column({
+    length: 100,
+    nullable: false,
+  })
+  name: string;
+
+  @Column({
+    enum: CategoryEnum,
+  })
+  category: CategoryEnum;
+
+  @Column({
+    type: 'int',
+    nullable: false,
+  })
+  price: number;
 }
