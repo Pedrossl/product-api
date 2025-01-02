2f815bf93f62e2d189cf89bcb962a992b3304f9d
Lobato <pedrosslobato@gmail.com>
2025-01-02 08:53:30 -0300

    🥅 Add error constants

---
+++ src/common/constants/error.constants.ts
@@ -0,0 +1,8 @@
+// Tokens
+export const TOKEN_NOT_FOUND = 'Token not found';
+export const INVALID_CONTEXT_TYPE = 'Invalid context type';
+
+// Users
+export const INVALID_CREDENTIALS = 'Invalid credentials';
+export const USER_NOT_FOUND = 'User not found';
+export const USER_NOT_CREATED = 'User not created';
