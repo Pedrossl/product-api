1e1cda8e82afe26a989d7a17928c2ab8231dd22b
Lobato <pedrosslobato@gmail.com>
2025-01-02 23:21:22 -0300

    🔧 Update JWT Expiration Time To 24 Hours

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -10,7 +10,7 @@ import { AuthController } from './auth.controller';
     UserModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1h' },
+      signOptions: { expiresIn: '24h' },
     }),
   ],
   providers: [AuthService],
