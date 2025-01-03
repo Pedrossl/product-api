5c36d1482d8905bfd2d65d054343289ccaeffa0d
Lobato <pedrosslobato@gmail.com>
2025-01-02 23:26:45 -0300

    🥅 Add Product Error Messages

--- src/common/constants/error.constants.ts
+++ src/common/constants/error.constants.ts
@@ -6,3 +6,8 @@ export const INVALID_CONTEXT_TYPE = 'Invalid context type';
 export const INVALID_CREDENTIALS = 'Invalid credentials';
 export const USER_NOT_FOUND = 'User not found';
 export const USER_NOT_CREATED = 'User not created';
+export const USER_EXISTS = 'User already exists';
+
+// Products
+export const PRODUCT_NOT_FOUND = 'Product not found';
+export const PRODUCT_NOT_CREATED = 'Product not created';
