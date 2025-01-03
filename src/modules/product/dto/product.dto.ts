f3950a7603ad6d520791a978332f77bf37aeac1a
Lobato <pedrosslobato@gmail.com>
2025-01-02 23:25:00 -0300

    ✨ Add ProductPageDTO for pagination support in product listing

--- src/modules/product/dto/product.dto.ts
+++ src/modules/product/dto/product.dto.ts
@@ -1,6 +1,7 @@
 import { Field, Float, ObjectType } from '@nestjs/graphql';
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { CategoryEnum } from 'src/common/enuns/category.enum';
+import { PageInfoDTO } from 'src/common/pagination/dto/page-info.dto';
 import { BaseDTO } from 'src/modules/bases/dto/base.dto';
 
 @ObjectType('ProductDTO')
@@ -19,4 +20,12 @@ export class ProductDTO extends BaseDTO {
   @IsNotEmpty()
   @IsNumber()
   price: number;
+}
+@ObjectType('ProductPageDTO')
+export class ProductPageDTO {
+  @Field(() => [ProductDTO])
+  nodes: ProductDTO[];
+
+  @Field(() => PageInfoDTO)
+  pageInfo: PageInfoDTO;
 }
