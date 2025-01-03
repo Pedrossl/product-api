de6826f3239e1f9e8c8ef69366f6fdd5cd41f7d3
Lobato <pedrosslobato@gmail.com>
2025-01-02 23:18:00 -0300

    ➕ Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
     }),
   );
-  await app.listen(process.env.API_PORT);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Product API')
+    .setDescription('API for managing products, categories, and prices')
+    .setVersion('1.0')
+    .addTag('User')
+    .addTag('Auth')
+    .addBearerAuth()
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, swaggerDocument);
+
+  const port = process.env.API_PORT || 4000;
+  await app.listen(port);
 }
+
 bootstrap();
