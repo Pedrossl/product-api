8b927ace9ddc55a4cbe6035eaa2fbf491021c1af
Lobato <pedrosslobato@gmail.com>
2025-01-02 23:25:54 -0300

    ♻️ Add UserController To User Module And Remove UserResolver

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
-import { UserResolver } from './user.resolver';
+
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UserResolver, UserService],
+  providers: [UserService],
+  controllers: [UserController],
   exports: [UserService],
 })
 export class UserModule {}
